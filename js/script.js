29f5840baa765064ba5636a31e5851dacad19c2c
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2021-06-13 11:02:12 +0600

    form validation updated

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 //first step
 //import Html using DOM to access form js file
 let form=document.querySelector('#task-form');
-let taskList=document.querySelector("ul");
-let filter=document.querySelectory("#task-filter");
-let taskInput=document.querySelector("#new-task");
-let clearBtn=document.querySelector("#clear-task-btn");
+let taskInput=document.querySelector('#new-task');
+let filter=document.querySelector('#task-filter');
+let taskList=document.querySelector('ul');
+let clearBtn=document.querySelector('#clear-task-btn');
 
 //Defining Event listener
 form.addEventListener('submit',addTask);
@@ -14,11 +14,21 @@ form.addEventListener('submit',addTask);
 //defining function
 //addTask function
 function addTask(e){
-    if(taskInput.value=='')//checking the task Input value is empty or not
+    if(taskInput.value=== '')//checking the task Input value is empty or not
     {
         alert("Add a task!");
-    }else{
-        //create a li element
-        let li=document.createElement('li')
     }
+     else{
+        //create a li element
+        let li=document.createElement('li');
+         li.appendChild(document.createTextNode(taskInput.value + ' '));
+        let link=document.createElement('a');
+        link.setAttribute('href','#');
+        link.innerHTML='x';
+        li.appendChild(link);
+         //1.inserting into task list
+         taskList.appendChild(li);
+         taskInput.value= ' ';//task list will be empty
+        }
+        e.preventDefault();//to prevent the default system of form as it keep reloading
 }
