90534cde2b1bd0afe2e887e65621792ddf0e1f31
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2021-06-13 19:48:06 +0600

    add remove and clear complete

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ let clearBtn=document.querySelector('#clear-task-btn');
 
 //Defining Event listener
 form.addEventListener('submit',addTask);
-
-
+taskList.addEventListener('click',removeTask);
+clearBtn.addEventListener('click',clearTask);
 //defining function
 //addTask function
 function addTask(e){
@@ -23,6 +23,7 @@ function addTask(e){
         let li=document.createElement('li');
          li.appendChild(document.createTextNode(taskInput.value + ' '));
         let link=document.createElement('a');
+        //2.creating delete button
         link.setAttribute('href','#');
         link.innerHTML='x';
         li.appendChild(link);
@@ -31,4 +32,22 @@ function addTask(e){
          taskInput.value= ' ';//task list will be empty
         }
         e.preventDefault();//to prevent the default system of form as it keep reloading
+}
+
+
+///remove task function
+function removeTask(e){
+    if(e.target.hasAttribute("href"))
+    if(confirm("Are you sure?")){
+        let ele=e.target.parentElement;
+        console.log(ele);
+        ele.remove();
+    }
+
+  }
+    
+//clear function
+
+function clearTask(e) {
+    taskList.innerHTML="";
 }
